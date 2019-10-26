bad72df500163d9c662d93b4657d9a987789c660
Kunwar Sahni <kunwar.sahni01@gmail.com>
2019-10-26 17:11:59 -0400

    Added Header File

---
+++ selector.h
@@ -0,0 +1,23 @@
+#ifndef _SELECTOR_H_
+#define _SELECTOR_H_
+
+
+#include "api.h"
+class selector
+{
+public:
+
+static lv_res_t redBtnmAction(lv_obj_t *btnm, const char *txt);
+
+
+static lv_res_t blueBtnmAction(lv_obj_t *btnm, const char *txt);
+
+
+static lv_res_t skillsBtnAction(lv_obj_t *btn);
+
+};
+
+void selectorInit(void);
+
+
+#endif
