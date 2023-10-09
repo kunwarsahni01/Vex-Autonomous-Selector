ee94d8a524e26988f2c1a41cee5dd2d06869a8f4
Adam Salem <84548368+Adam-Salem-Codes@users.noreply.github.com>
2023-10-09 00:44:03 -0400

    Update selection.h

--- selection.h
+++ selection.h
@@ -1,16 +1,18 @@
 #pragma once
 
 #include <string>
+#include "main.h"
 
-//selector configuration
-#define HUE 360
+// selector configuration
+#define HUE 130
 #define DEFAULT 1
 #define AUTONS "Front", "Back", "Do Nothing"
 
-namespace selector{
+namespace selector
+{
 
-extern int auton;
-const char *b[] = {AUTONS, ""};
-void init(int hue = HUE, int default_auton = DEFAULT, const char **autons = b);
+    extern int auton;
+    extern const char *b[];
+    void init(int hue = HUE, int default_auton = DEFAULT, const char **autons = b);
 
 }
